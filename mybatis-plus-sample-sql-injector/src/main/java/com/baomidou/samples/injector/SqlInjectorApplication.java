1a26211c3cc0861b0f0fa3780af6cc7a1b56c970
zhaishipeng <1793172336@qq.com>
2025-02-12 14:36:01 +0800

    sql injector

--- mybatis-plus-sample-sql-injector/src/main/java/com/baomidou/samples/injector/SqlInjectorApplication.java
+++ mybatis-plus-sample-sql-injector/src/main/java/com/baomidou/samples/injector/SqlInjectorApplication.java
@@ -2,16 +2,17 @@ package com.baomidou.samples.injector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动器
  *
  * @author nieqiurong 2018/8/11 20:32.
  */
-@SpringBootApplication
-public class Application {
+@SpringBootApplication()
+public class SqlInjectorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(SqlInjectorApplication.class, args);
     }
 }
